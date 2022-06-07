3d40f685b4755fb82f86a88372324e3488b9514d
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-07 15:57:46 -0400

    updating side bar content

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import{ myworksArray } from './data/app2.js'
 // myworksArray.forEach(work=>
 //     console.log(work)
 // )
-console.log(myworksArray)
+// console.log(myworksArray)
+lightDarkBtn.addEventListener("click", toggleLightDark)
 
 
+function toggleLightDark() {
+    body.className = body.className === "dark" ? "" : "dark"
+  }
+  
+  function checkDarkPref() {
+    if(
+      window.matchMedia("(prefers-color-scheme:dark)").matches && 
+      body.className !== "dark"
+    ) {
+      toggleLightDark()
+    }
+  }
+  checkDarkPref()  
--- data/app2.js
+++ data/app2.js
@@ -1,38 +1,38 @@
 const myworksArray = [
-{
-    image: "Snapshot of TTT",
-    title: "Tic Tac Toe",
-    description:"My first game, Tic Tac Toe!",
-    githubLink:"Here is my GH Link",
-    deploymentLink:"Here is the Site",
-},
-{
-    image: "Snapshot of Guess the Number Game",
-    title: "Guess The Number Game",
-    description:"Guess The Number Project",
-    githubLink:"Here is my GH Link",
-    deploymentLink:"Here is the Site",
-
-
-}
-
-
-]
-export {
-    myworksArray
-    
-}
-let modalBtn = document.getElementById("modal-btn")
-let modal = document.querySelector(".modal")
-let closeBtn = document.querySelector(".close-btn")
-modalBtn.onclick = function(){
-  modal.style.display = "block"
-}
-closeBtn.onclick = function(){
-  modal.style.display = "none"
-}
-window.onclick = function(e){
-  if(e.target == modal){
+  {
+      image: "Snapshot of TTT",
+      title: "Tic Tac Toe",
+      description:"My first game, Tic Tac Toe!",
+      githubLink:"Here is my GH Link",
+      deploymentLink:"Here is the Site",
+  },
+  {
+      image: "Snapshot of Guess the Number Game",
+      title: "Guess The Number Game",
+      description:"Guess The Number Project",
+      githubLink:"Here is my GH Link",
+      deploymentLink:"Here is the Site",
+  
+  
+  }
+  
+  
+  ]
+  export {
+      myworksArray
+      
+  }
+  let modalBtn = document.getElementById("modal-btn")
+  let modal = document.querySelector(".modal")
+  let closeBtn = document.querySelector(".close-btn")
+  modalBtn.onclick = function(){
+    modal.style.display = "block"
+  }
+  closeBtn.onclick = function(){
     modal.style.display = "none"
   }
-}
+  window.onclick = function(e){
+    if(e.target == modal){
+      modal.style.display = "none"
+    }
+  }
