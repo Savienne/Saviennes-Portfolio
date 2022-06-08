2de448ad8f00dc61b1da44a210d960679b844c46
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-08 17:31:17 -0400

    troubleshooted portfolio

---
+++ js/portfolio-data.js
@@ -0,0 +1,49 @@
+const myworksArray = [
+  {
+      image: "Snapshot of TTT",
+      title: "Tic Tac Toe",
+      description:"My first game, Tic Tac Toe!",
+      githubLink:"Here is my GH Link",
+      deploymentLink:"Here is the Site",
+  },
+  {
+      image: "Snapshot of Guess the Number Game",
+      title: "Guess The Number Game",
+      description:"Guess The Number Project",
+      githubLink:"Here is my GH Link",
+      deploymentLink:"Here is the Site",
+  
+  
+  }
+  
+  
+  ]
+  export {
+      myworksArray
+      
+  }
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+  }
+  closeBtn.onclick = function(){
+    modal.style.display = "none"
+  }
+  window.onclick = function(e){
+    if(e.target == modal){
+      modal.style.display = "none"
+    }
+  }
+
+  const portfoliosound = new Audio('../assets/audio/Future.mp3')
+  // console.log(modalBtn)
+  modalBtn.addEventListener('click', function(evt){
+    console.log(evt.target)
+    portfoliosound.volume = .10
+    portfoliosound.play()
+  })
+    
+
+  
